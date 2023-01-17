72856c95ed545a0290d973b23b31c3b935ef65f4
Luiz Barbosa <luizppbarbosa@gmail.com>
2023-01-17 16:02:50 -0300

    att 17-01-23

--- lesson-1/Linear-Binary-Search.py
+++ lesson-1/Linear-Binary-Search.py
@@ -9,6 +9,25 @@ def locate_card_BF (cards, query):
     
     return -1
 
+
+def locate_card_binary(cards, query):
+    lo, hi = 0, len(cards) - 1
+    
+    while lo <= hi:
+        mid = (lo + hi) // 2
+        mid_number = cards[mid]
+        
+        print("lo:", lo, ", hi:", hi, ", mid:", mid, ", mid_number:", mid_number)
+        
+        if mid_number == query:
+            return mid
+        elif mid_number < query:
+            hi = mid - 1  
+        elif mid_number > query:
+            lo = mid + 1
+    
+    return -1
+
     
 
 tests = []
@@ -94,6 +113,7 @@ tests.append({
 
 evaluate_test_cases(locate_card_BF, tests)
 
+evaluate_test_cases(locate_card_binary, tests)
 
 
 
